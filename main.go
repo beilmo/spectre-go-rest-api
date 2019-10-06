c44df41c7519e6546b2068aa583fa78eb69867d5
Dorin-Bogdan Danciu <1619134+dorindanciu@users.noreply.github.com>
2019-10-06 18:34:10 +0300

    Link github.com/beilmo/spectre-go-entity package

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"net/http"
 
+	entity "github.com/beilmo/spectre-go-entity"
+
+	"github.com/golang/protobuf/proto"
 	"github.com/gorilla/mux"
 )
 
@@ -13,6 +16,32 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	speaker := &entity.Speaker{
+		Id:        101,
+		FirstName: "Dorin",
+		LastName:  "Danciu",
+	}
+
+	data, err := proto.Marshal(speaker)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+
+	newSpeaker := &entity.Speaker{}
+	err = proto.Unmarshal(data, newSpeaker)
+	if err != nil {
+		log.Fatal("unmarshaling error: ", err)
+	}
+	// Now test and newTest contain the same data.
+	if speaker.GetFirstName() != newSpeaker.GetFirstName() {
+		log.Fatalf("data mismatch %q != %q", speaker.GetFirstName(), newSpeaker.GetFirstName())
+	}
+
+	fmt.Println("Hello World!")
+	i := 101
+	fmt.Println(i)
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
 	log.Fatal(http.ListenAndServe(":8080", router))
